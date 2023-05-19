ed8c80cf12644e7a8b2c43b630a13bcbe614c3f5
Jack Mabie <johnmabie94@gmail.com>
2023-05-18 22:30:28 -0500

    ran prettier

--- server/models/Game.js
+++ server/models/Game.js
@@ -2,20 +2,20 @@ const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 const game = new Schema({
-    name: {
-        type: String,
-        minlength: 1,
-        maxlength: 120,
-        required: true,
-    },
-    numPlayersRequired: {
-        type: Number,
-        required: true,
-    },
-    isActive: {
-        type: Boolean,
-        required: true,
-    }
+  name: {
+    type: String,
+    minlength: 1,
+    maxlength: 120,
+    required: true,
+  },
+  numPlayersRequired: {
+    type: Number,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+  },
 });
 
 const Game = model('Game', gameSchema);
