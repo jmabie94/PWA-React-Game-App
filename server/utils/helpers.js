3c0063824df5aec9008e02373c112a628ee071f2
Jack Mabie <johnmabie94@gmail.com>
2023-05-18 22:35:51 -0500

    renaming database, adding dateFormat

---
+++ server/utils/helpers.js
@@ -0,0 +1,98 @@
+// cloned dateFormat from NoSQL Social Network API project
+
+const addSuffix = date => {
+    let dateString = date.toString();
+
+    const lastCharacter = dateString.charAt(dateString.length - 1);
+
+    // forgot to add triple equals
+    if (lastCharacter === '1' && dateString !== '11') {
+        dateString = `${dateString}st`;
+    } else if (lastCharacter === '2' && dateString !== '12') {
+        dateString = `${dateString}nd`;
+    } else if (lastCharacter === '3' && dateString !== '13') {
+        dateString = `${dateString}rd`;
+    } else {
+        dateString = `${dateString}th`;
+    }
+
+    return dateString;
+};
+
+module.exports = (
+    timestamp,
+    { monthLength = 'short', dateSuffix = true } = {}
+) => {
+    let months;
+
+    if (monthLength === 'short') {
+        months = {
+            0: 'Jan',
+            1: 'Feb',
+            2: 'Mar',
+            3: 'Apr',
+            4: 'May',
+            5: 'Jun',
+            6: 'Jul',
+            7: 'Aug',
+            8: 'Sep',
+            9: 'Oct',
+            10: 'Nov',
+            11: 'Dec'
+        };
+    } else {
+        months = {
+            0: 'January',
+            1: 'February',
+            2: 'March',
+            3: 'April',
+            4: 'May',
+            5: 'June',
+            6: 'July',
+            7: 'August',
+            8: 'September',
+            9: 'October',
+            10: 'November',
+            11: 'December'
+        };
+    }
+
+    const dateObj = new Date(timestamp);
+    const formatMonth = months[dateObj.getMonth()];
+
+    let dayOfMonth;
+
+    if (dateSuffix) {
+        dayOfMonth = addSuffix(dateObj.getDate());
+    } else {
+        dayOfMonth = dateObj.getDate();
+    }
+
+    const year = dateObj.getFullYear();
+
+    let hour;
+
+    if (dateObj.getHours > 12) {
+        hour = Math.floor(dateObj.getHours() / 2);
+    } else {
+        hour = dateObj.getHours();
+    }
+
+    if (hour === 0) {
+        hour = 12;
+    }
+
+    const minutes = dateObj.getMinutes();
+
+    let periodOfDay;
+
+    if (dateObj.getHours() >= 12) {
+        periodOfDay = 'pm';
+    } else {
+        periodOfDay = 'am';
+    }
+
+    const formatTimeStamp = `${formatMonth} ${dayOfMonth}, ${year} at ${hour}:${minutes} ${periodOfDay}`;
+
+    return formatTimeStamp;
+};
