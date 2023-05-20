790aac242c1de4e33523f883126a79212dd83f2d
Jordan Pace <jordanpace95@gmail.com>
2023-05-20 17:15:29 -0600

    temporarily fixes login/signup form

--- client/src/components/form/FormContainer.js
+++ client/src/components/form/FormContainer.js
@@ -7,36 +7,36 @@ import { LOGIN_USER } from '../../utils/mutations';
 import AuthService from '../../utils/auth.js'
 
 const FormContainer = () => {
-    const [formState, setFormState] = useState({ email: '', password: '' });
-    const [login, { error, data }] = useMutation(LOGIN_USER);
-    // const [login, setLogin] = useState(true);
+    // const [formState, setFormState] = useState({ email: '', password: '' });
+    // const [login, { error, data }] = useMutation(LOGIN_USER);
+    const [login, setLogin] = useState(true);
 
     const ref = useRef(null);
 
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-        console.log(formState);
-        try {
-          const { data } = await login({
-            variables: { ...formState },
-          });
+    // const handleFormSubmit = async (event) => {
+    //     event.preventDefault();
+    //     console.log(formState);
+    //     try {
+    //       const { data } = await login({
+    //         variables: { ...formState },
+    //       });
     
-          AuthService.login(data.login.token);
-        } catch (e) {
-          console.error(e);
-        }
+    //       AuthService.login(data.login.token);
+    //     } catch (e) {
+    //       console.error(e);
+    //     }
     
-        // clear form values
-        setFormState({
-          email: '',
-          password: '',
-        });
-      };
+    //     // clear form values
+    //     setFormState({
+    //       email: '',
+    //       password: '',
+    //     });
+    //   };
 
-    // const handleFormSubmit = () => {
-    //     setLogin(!login);
-    //     ref.current.classList.toggle('active');
-    // }
+    const handleFormSubmit = () => {
+        setLogin(!login);
+        ref.current.classList.toggle('active');
+    }
 
 
     return (
