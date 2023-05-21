e59f9f5366440ff85b6569328ce9018473184bcd
Jorge Gomez <jorge.electron@gmail.com>
2023-05-21 14:29:03 -0700

    adds a function to match cards

--- client/src/components/games/memory/script.js
+++ client/src/components/games/memory/script.js
@@ -1,8 +1,26 @@
 const cards = document.querySelectorAll(".card");
 
-function flipCard(e) {
+let cardOne, cardTwo;
+
+function flipCard(e) {    
     let clickedCard = e.target;
-    clickedCard.classList.add("flip");
+    if(clickedCard !== cardOne){
+        clickedCard.classList.add("flip");
+        if(!cardOne) {
+            return cardOne = clickedCard;
+        }
+        cardTwo = clickedCard;
+
+        let cardOneImg = cardOne.querySelector(".back-view img").src;
+        let cardTwoImg = cardTwo.querySelector(".back-view img").src;
+        matchCards(cardOneImg, cardTwoImg);
+    }
+}
+
+function matchCards(img1, img2) {
+    if(img1 === img2) {
+            console.log('img1, img2 :>> ', img1, img2);
+    }
 }
 
 cards.forEach(card => {
