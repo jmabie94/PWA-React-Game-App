1c7d9982b1d1db3021bdfdc5be5678cf8bd96791
rhubarb414 <ctophr.dubois@gmail.com>
2023-05-20 19:58:23 -0700

    user must be logged in to view profile page

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,10 +3,10 @@ import './profile.css';
 import Auth from '../../utils/auth';
 import { useQuery } from '@apollo/client';
 import { QUERY_SINGLE_USER } from '../../utils/queries';
-import { QUERY_ALL_USERS } from '../../utils/queries';
 
 export default function Profile() {
   const username = 'Kent Beck'; //using as an example until login implementation works
+
   const { loading, data, error } = useQuery(QUERY_SINGLE_USER, {
     variables: { username },
   });
@@ -18,18 +18,26 @@ export default function Profile() {
   if (loading) {
     return <h2>LOADING...</h2>;
   }
+
   return (
     <div>
-      <h1>Welcome, {userData.username}</h1>
+      {/* if user is logged in, show profile. Otherwise, show "log in" message */}
+      {Auth.loggedIn() ? (
+        <>
+          <h1>Welcome, {userData.username}</h1>
 
-      <div className="stats-container">
-        <h2>Your Stats</h2>
-        <ul>
-          <li>Tic-Tac-Toe Games Played: </li>
-          <li>Wins/Losses: </li>
-          <li></li>
-        </ul>
-      </div>
+          <div className="stats-container">
+            <h2>Your Stats</h2>
+            <ul>
+              <li>Tic-Tac-Toe Games Played: </li>
+              <li>Wins/Losses: </li>
+              <li></li>
+            </ul>
+          </div>
+        </>
+      ) : (
+        <h1>You must be logged in!</h1>
+      )}
     </div>
   );
 }
