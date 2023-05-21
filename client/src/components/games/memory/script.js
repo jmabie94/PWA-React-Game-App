a44d77739369861b84bfe6fff90a3caaff7434e7
Jorge Gomez <jorge.electron@gmail.com>
2023-05-21 14:36:10 -0700

    adds login for timer and matching

--- client/src/components/games/memory/script.js
+++ client/src/components/games/memory/script.js
@@ -1,6 +1,10 @@
 const cards = document.querySelectorAll(".card");
-
+let disableDeck = false;
+let matchedCard = 0;
 let cardOne, cardTwo;
+let maxTime = 40;
+let timeLeft = maxTime;
+
 
 function flipCard(e) {    
     let clickedCard = e.target;
@@ -19,8 +23,27 @@ function flipCard(e) {
 
 function matchCards(img1, img2) {
     if(img1 === img2) {
-            console.log('img1, img2 :>> ', img1, img2);
+            //console.log('img1, img2 :>> ', img1, img2);
+        matchedCard++;
+        if(matchedCard == cards.length && timeLeft > 0) {
+            return clearInterval(timer);
+        }
+        cardOne.removeEventListener("click", flipCard);
+        cardTwo.removeEventListener("click", flipCard);
+        cardOne = cardTwo = "";
+        return disableDeck = false;
     }
+    setTimeout(() => {
+        cardOne.classList.add("shake");
+        cardTwo.classList.add("shake");
+    }, 400);
+
+    setTimeout(() => {
+        cardOne.classList.remove("shake", "flip");
+        cardTwo.classList.remove("shake", "flip");
+        cardOne = cardTwo = "";
+        disableDeck = false;
+    }, 1200);
 }
 
 cards.forEach(card => {
