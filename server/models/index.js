6db8407e27478fbfb7e7da0f0fa6e2d4abf455f7
rhubarb414 <ctophr.dubois@gmail.com>
2023-05-24 21:30:53 -0700

    remove Record from models/index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,6 @@
 const User = require('./User');
 const Game = require('./Game');
 const Session = require('./Session');
-const Record = require('./Record');
 
 // now all models are connected properly
-module.exports = { User, Game, Session, Record };
+module.exports = { User, Game, Session };
